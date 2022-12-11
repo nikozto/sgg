b2d590fab0591f881c76c22b2d7d2467d8fde30c
duck <duck@rain.com>
2022-12-11 12:19:12 +0800

    second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 class HelloWorld {
     public static void main(String[] args){
         System.out.println("xuexihagitdeshiyong");
+        System.out.println("second");
+        System.out.println("second");
     }
 }
